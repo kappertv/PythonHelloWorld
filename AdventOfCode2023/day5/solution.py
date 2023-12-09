550f8415b5e436062f7eb4a9fea7db4c9244fa23
Vincent Kappert <vincent.kappert@gmail.com>
2023-12-09 23:07:47 +0100

    solve day 5 with a little help from github for part 2

--- AdventOfCode2023/day5/solution.py
+++ AdventOfCode2023/day5/solution.py
@@ -1,17 +1,99 @@
-class Car:
-    def __init__(self, color, mileage, automatic):
-        self.color = color
-        self.mileage = mileage
-        self.automatic = automatic
+
+def apply(x, map) -> int:
+    # dest, src, range
+    for m in map:
+        dest = m[0]
+        src = m[1]
+        range = m[2]
+        # print(f'{dest}, {src}, {range}, {x}')
+        if (x >= src and x < (src + range)):
+            # print('true')
+            return x + dest - src
+    
+    return x
+
         
-with open("sampleinput") as f:
-    data = f.read()
-    lines = data.split("\n")
+def getLocation(seed, mSoil, mFert, mWater, mLight, mTemp, mHum, mLoc) -> int:
+    soil = apply(seed, mSoil)
+    fert = apply(soil, mFert)
+    water = apply(fert, mWater)
+    light = apply(water, mLight)
+    temp = apply(light, mTemp)
+    hum = apply(temp, mHum)
+    loc = apply(hum, mLoc)
+    print(f'{soil}, {fert}, {water}, {light}, {temp}, {temp}, {hum}, {loc}')
+    return loc    
 
 
-def lowestLocationNumber() -> int:
-    print(lines)
+def getMap(lines) -> []:
+    map = []
+    for l in lines:
+        map.append([int(elem) for elem in l.split()])
+    return map
+
+
+
+def part2(input) -> int:
+    with open(input) as f:
+        data = f.read()
+        lines = data.split("\n")
+
+    sr = [int(elem) for elem in lines[0][7:].split()]
+    seeds = []
+    
+    for i,r in zip(sr[0::2], sr[1::2]):
+        for j in range(r):
+            seeds.append(i+j)
+    print(len(seeds))
+    print(seeds)
+    
     return 0
+    
+    iSoil = lines.index('seed-to-soil map:')
+    iFert = lines.index('soil-to-fertilizer map:')
+    iWater = lines.index('fertilizer-to-water map:')
+    iLight = lines.index('water-to-light map:')
+    iTemp = lines.index('light-to-temperature map:')
+    iHum = lines.index('temperature-to-humidity map:')
+    iLoc = lines.index('humidity-to-location map:')
+    
+    mSoil = getMap(lines[iSoil+1:iFert-1])
+    mFert = getMap(lines[iFert+1:iWater-1])
+    mWater = getMap(lines[iWater+1:iLight-1])
+    mLight = getMap(lines[iLight+1:iTemp-1])
+    mTemp = getMap(lines[iTemp+1:iHum-1])
+    mHum = getMap(lines[iHum+1:iLoc-1])
+    mLoc = getMap(lines[iLoc+1:])
 
-def part1() -> int:
-    return lowestLocationNumber()
+    locations = []
+    for s in seeds:
+        locations.append(getLocation(s, mSoil, mFert, mWater, mLight, mTemp, mHum, mLoc))
+    return min(locations)
+
+def part1(input) -> int:
+    with open(input) as f:
+        data = f.read()
+        lines = data.split("\n")
+
+    seeds = [int(elem) for elem in lines[0][7:].split()]
+    
+    iSoil = lines.index('seed-to-soil map:')
+    iFert = lines.index('soil-to-fertilizer map:')
+    iWater = lines.index('fertilizer-to-water map:')
+    iLight = lines.index('water-to-light map:')
+    iTemp = lines.index('light-to-temperature map:')
+    iHum = lines.index('temperature-to-humidity map:')
+    iLoc = lines.index('humidity-to-location map:')
+    
+    mSoil = getMap(lines[iSoil+1:iFert-1])
+    mFert = getMap(lines[iFert+1:iWater-1])
+    mWater = getMap(lines[iWater+1:iLight-1])
+    mLight = getMap(lines[iLight+1:iTemp-1])
+    mTemp = getMap(lines[iTemp+1:iHum-1])
+    mHum = getMap(lines[iHum+1:iLoc-1])
+    mLoc = getMap(lines[iLoc+1:])
+
+    locations = []
+    for s in seeds:
+        locations.append(getLocation(s, mSoil, mFert, mWater, mLight, mTemp, mHum, mLoc))
+    return min(locations)
--- AdventOfCode2023/day5/test.py
+++ AdventOfCode2023/day5/test.py
@@ -4,5 +4,13 @@ from solution import *
 class MyFirstTests(unittest.TestCase):
 
     def test_part1(self):
-        self.assertEqual(part1(), 35)
+        self.assertEqual(part1("sampleinput"), 35)
+
+    def test_part1i(self):
+        self.assertEqual(part1("input"), 226172555)
     
+    def test_part2(self):
+        self.assertEqual(part2("sampleinput"), 46)
+
+    def test_part2i(self):
+        self.assertEqual(part2("input"), 46)
